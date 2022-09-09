dac9ce53ace10558556f2f30ee4d8b6922369888
Angel Rosas <marosascabrera@gmail.com>
2022-09-08 23:28:36 -0500

    Evitando valores nulos en los modelos de las respuestas del servidor.

--- app/src/main/java/com/example/bitsocurrency/data/services/models/BitsoResponse.kt
+++ app/src/main/java/com/example/bitsocurrency/data/services/models/BitsoResponse.kt
@@ -4,35 +4,35 @@ import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 data class BitsoResponse(
-    @SerializedName("payload") @Expose val payload: List<BitsoModel>,
-    @SerializedName("success") @Expose val success: Boolean
+    @SerializedName("payload") @Expose val payload: List<BitsoModel> = listOf(),
+    @SerializedName("success") @Expose val success: Boolean = false,
 )
 
 data class BitsoModel(
-    @SerializedName("book") @Expose val book: String,
-    @SerializedName("default_chart") @Expose val defaultChart: String,
-    @SerializedName("fees") @Expose val fees: FeesModel,
-    @SerializedName("maximum_amount") @Expose val maximumAmount: String,
-    @SerializedName("maximum_price") @Expose val maximumPrice: String,
-    @SerializedName("maximum_value") @Expose val maximumValue: String,
-    @SerializedName("minimum_amount") @Expose val minimumAmount: String,
-    @SerializedName("minimum_price") @Expose val minimumPrice: String,
-    @SerializedName("minimum_value") @Expose val minimumValue: String,
-    @SerializedName("tick_size") @Expose val tickSize: String
+    @SerializedName("book") @Expose val book: String = "",
+    @SerializedName("default_chart") @Expose val defaultChart: String = "",
+    @SerializedName("fees") @Expose val fees: FeesModel = FeesModel(),
+    @SerializedName("maximum_amount") @Expose val maximumAmount: String = "",
+    @SerializedName("maximum_price") @Expose val maximumPrice: String = "",
+    @SerializedName("maximum_value") @Expose val maximumValue: String = "",
+    @SerializedName("minimum_amount") @Expose val minimumAmount: String = "",
+    @SerializedName("minimum_price") @Expose val minimumPrice: String = "",
+    @SerializedName("minimum_value") @Expose val minimumValue: String = "",
+    @SerializedName("tick_size") @Expose val tickSize: String = "",
 )
 
 data class FeesModel(
-    @SerializedName("flat_rate") @Expose val flatRate: FlatRateModel,
-    @SerializedName("structure") @Expose val structure: List<StructureModel>
+    @SerializedName("flat_rate") @Expose val flatRate: FlatRateModel = FlatRateModel(),
+    @SerializedName("structure") @Expose val structure: List<StructureModel> = listOf(),
 )
 
 data class FlatRateModel(
-    @SerializedName("maker") @Expose val maker: String,
-    @SerializedName("taker") @Expose val taker: String
+    @SerializedName("maker") @Expose val maker: String = "",
+    @SerializedName("taker") @Expose val taker: String = "",
 )
 
 data class StructureModel(
-    @SerializedName("maker") @Expose val maker: String,
-    @SerializedName("taker") @Expose val taker: String,
-    @SerializedName("volume") @Expose val volume: String
+    @SerializedName("maker") @Expose val maker: String = "",
+    @SerializedName("taker") @Expose val taker: String = "",
+    @SerializedName("volume") @Expose val volume: String = "",
 )
--- app/src/main/java/com/example/bitsocurrency/data/services/models/BookModel.kt
+++ app/src/main/java/com/example/bitsocurrency/data/services/models/BookModel.kt
@@ -3,27 +3,26 @@ package com.example.bitsocurrency.data.services.models
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-
 data class BookResponse(
-    @SerializedName("payload") @Expose val payload: BookModel,
-    @SerializedName("success") @Expose val success: Boolean
+    @SerializedName("payload") @Expose val payload: BookModel = BookModel(),
+    @SerializedName("success") @Expose val success: Boolean = false,
 )
 
 data class BookModel(
-    @SerializedName("asks") @Expose val asks: List<AskModel>,
-    @SerializedName("bids") @Expose val bids: List<BidModel>,
-    @SerializedName("sequence") @Expose val sequence: String,
-    @SerializedName("updated_at") @Expose val updatedAt: String
+    @SerializedName("asks") @Expose val asks: List<AskModel> = listOf(),
+    @SerializedName("bids") @Expose val bids: List<BidModel> = listOf(),
+    @SerializedName("sequence") @Expose val sequence: String = "",
+    @SerializedName("updated_at") @Expose val updatedAt: String = "",
 )
 
 data class AskModel(
-    @SerializedName("amount") @Expose val amount: String,
-    @SerializedName("book") @Expose val book: String,
-    @SerializedName("price") @Expose val price: String
+    @SerializedName("amount") @Expose val amount: String = "",
+    @SerializedName("book") @Expose val book: String = "",
+    @SerializedName("price") @Expose val price: String = "",
 )
 
 data class BidModel(
-    @SerializedName("amount") @Expose val amount: String,
-    @SerializedName("book") @Expose val book: String,
-    @SerializedName("price") @Expose val price: String
+    @SerializedName("amount") @Expose val amount: String = "",
+    @SerializedName("book") @Expose val book: String = "",
+    @SerializedName("price") @Expose val price: String = "",
 )

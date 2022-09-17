13e2c6783f6fdcceb0e674af9bd077e018bec738
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-17 16:55:53 -0500

    Configurando fragment y vista de la lista de Bitso.

---
+++ app/src/main/java/com/example/bitsocurrency/ui/fragments/BitsoFragment.kt
@@ -0,0 +1,42 @@
+package com.example.bitsocurrency.ui.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.bitsocurrency.databinding.FragmentBitsoBinding
+import com.example.bitsocurrency.ui.adapters.BitsoAdapter
+import com.example.bitsocurrency.ui.viewmodel.BitsoViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BitsoFragment : Fragment() {
+
+    private val viewModel: BitsoViewModel by activityViewModels()
+    private lateinit var binding: FragmentBitsoBinding
+    private lateinit var adapter: BitsoAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentBitsoBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        adapter = BitsoAdapter()
+
+        with(binding) {
+            rvBitsoCurrency.adapter = adapter
+            rvBitsoCurrency.setHasFixedSize(false)
+            rvBitsoCurrency.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
+        }
+
+        viewModel.getAvailableBooks()
+        viewModel.availableBooks.observe(this) { adapter.items = it }
+    }
+
+}

3296fa927c535782ccf47cae397176b57f26e054
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-17 16:16:41 -0500

    Agregando adapter para los items de bitso.

---
+++ app/src/main/java/com/example/bitsocurrency/ui/adapters/BitsoAdapter.kt
@@ -0,0 +1,33 @@
+package com.example.bitsocurrency.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.bitsocurrency.databinding.ListItemBitsoBinding
+import com.example.bitsocurrency.domain.models.Bitso
+import com.example.bitsocurrency.utils.extensions.diffUtil
+
+class BitsoAdapter : RecyclerView.Adapter<BitsoAdapter.BitsoViewHolder>() {
+
+    var items: List<Bitso> by diffUtil { old, new -> old.book == new.book }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BitsoViewHolder {
+        val binding = ListItemBitsoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return BitsoViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: BitsoViewHolder, position: Int) = holder.bind(items)
+
+    override fun getItemCount(): Int = items.size
+
+    inner class BitsoViewHolder(private val binding: ListItemBitsoBinding) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(items: List<Bitso>){
+            val currentItem = items[adapterPosition]
+            with(binding) {
+                tvMaximumAmount.text = currentItem.maximumAmount
+                tvMaximumPrice.text = currentItem.maximumPrice
+                tvNameCurrency.text = currentItem.book
+            }
+        }
+    }
+}

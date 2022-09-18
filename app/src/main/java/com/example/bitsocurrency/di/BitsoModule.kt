e6c159b578928ef2c4c74577e5173a99c6f8d61f
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-17 22:18:04 -0500

    Configurando servicio, repositorio de Coin, y dispatcher en el modulo de la app.

--- app/src/main/java/com/example/bitsocurrency/di/BitsoModule.kt
+++ app/src/main/java/com/example/bitsocurrency/di/BitsoModule.kt
@@ -1,13 +1,19 @@
 package com.example.bitsocurrency.di
 
-import com.example.bitsocurrency.BuildConfig
+import com.example.bitsocurrency.BuildConfig.API_COIN_MARKET
+import com.example.bitsocurrency.BuildConfig.API_URL
 import com.example.bitsocurrency.data.repository.BitsoRepository
 import com.example.bitsocurrency.data.repository.BitsoRepositoryImpl
+import com.example.bitsocurrency.data.repository.CoinRepository
+import com.example.bitsocurrency.data.repository.CoinRepositoryImpl
 import com.example.bitsocurrency.data.services.BitsoService
+import com.example.bitsocurrency.data.services.CoinService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -21,34 +27,48 @@ class BitsoModule {
 
     @Provides
     @Singleton
-    fun provideOkHttpClient(): OkHttpClient {
-        val interceptor = HttpLoggingInterceptor()
-        interceptor.level = HttpLoggingInterceptor.Level.BASIC
-        return OkHttpClient().newBuilder().addInterceptor(interceptor).build()
-    }
-
+    fun provideCoroutineDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
     @Provides
     @Singleton
-    fun provideRetrofit(client: OkHttpClient): Retrofit {
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient().newBuilder().addInterceptor(interceptor).build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(url: String, client: OkHttpClient): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(BuildConfig.API_URL)
+            .baseUrl(url)
             .client(client)
             .addConverterFactory(GsonConverterFactory.create())
             .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
             .build()
     }
 
-
     @Provides
     @Singleton
     fun provideBitsoService(): BitsoService {
-        return provideRetrofit(provideOkHttpClient()).create(BitsoService::class.java)
+        return provideRetrofit(API_URL, provideOkHttpClient()).create(BitsoService::class.java)
     }
 
     @Provides
     @Singleton
     fun provideBitsoRepository(): BitsoRepository {
-        return BitsoRepositoryImpl(provideBitsoService())
+        return BitsoRepositoryImpl(provideBitsoService(), provideCoroutineDispatcher())
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinService(): CoinService{
+        return provideRetrofit(API_COIN_MARKET, provideOkHttpClient()).create(CoinService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideCoinRepository(): CoinRepository {
+        return CoinRepositoryImpl(provideCoinService(), provideCoroutineDispatcher())
     }
 }

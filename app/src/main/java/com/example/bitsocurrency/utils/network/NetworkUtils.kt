d8715240b5a30a41f9dd0a16d6a327f4714e70da
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-21 20:21:12 -0500

    Agregando NetworkUtils para comprobar conexion a internet.

---
+++ app/src/main/java/com/example/bitsocurrency/utils/network/NetworkUtils.kt
@@ -0,0 +1,27 @@
+package com.example.bitsocurrency.utils.network
+
+import kotlinx.coroutines.coroutineScope
+import java.io.IOException
+import java.net.InetSocketAddress
+import java.net.Socket
+import java.net.SocketTimeoutException
+import java.nio.channels.IllegalBlockingModeException
+
+object Network {
+    suspend fun isNetworkAvailable():Boolean = coroutineScope {
+        return@coroutineScope try {
+            val socket = Socket()
+            val address = InetSocketAddress("8.8.8.8", 53)
+            val result = kotlin.runCatching {
+                socket.connect(address, 1500)
+                socket.close()
+            }
+            result.isSuccess
+        }
+        catch (e: Exception) { false }
+        catch (e: IOException) { false }
+        catch (e: SocketTimeoutException) { false }
+        catch (e: IllegalBlockingModeException) { false }
+        catch (e: IllegalArgumentException) { false }
+    }
+}

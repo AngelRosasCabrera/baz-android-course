ea24a0461ca7c920bbab3deae8473bade873a611
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-21 21:34:33 -0500

    Configurando AvailableBooksUseCase.

--- app/src/main/java/com/example/bitsocurrency/domain/usecases/AvailableBooksUseCase.kt
+++ app/src/main/java/com/example/bitsocurrency/domain/usecases/AvailableBooksUseCase.kt
@@ -1,24 +1,15 @@
 package com.example.bitsocurrency.domain.usecases
 
 import com.example.bitsocurrency.data.repository.BitsoRepository
-import com.example.bitsocurrency.data.repository.DatabaseRepository
 import com.example.bitsocurrency.domain.models.Bitso
 import javax.inject.Inject
 
-class AvailableBooksUseCase @Inject constructor(
-    private val repository: BitsoRepository,
-    private val databaseRepository: DatabaseRepository
-) {
+class AvailableBooksUseCase @Inject constructor(private val repository: BitsoRepository) {
     private var content: List<Bitso> = mutableListOf()
 
     suspend operator fun invoke(isRefresh: Boolean): List<Bitso> {
-        return if (isRefresh) fillList() else content.ifEmpty { databaseRepository.getAllData() }
-    }
+        if (isRefresh) content = repository.getAvailableBooks()
 
-    private suspend fun fillList(): List<Bitso> {
-        databaseRepository.deleteAllData()
-        content = repository.getAvailableBooks()
-        databaseRepository.insertData(content)
-        return content
+        return content.ifEmpty { repository.getAvailableBooks() }
     }
 }

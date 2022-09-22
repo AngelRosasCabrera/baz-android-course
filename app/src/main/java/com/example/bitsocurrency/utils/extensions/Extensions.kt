67e5b4a608aca96ae204e799066ffe93fac6ffd9
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-22 11:37:51 -0500

    Creando extensiones para formato de moneda y porcentaje.

--- app/src/main/java/com/example/bitsocurrency/utils/extensions/Extensions.kt
+++ app/src/main/java/com/example/bitsocurrency/utils/extensions/Extensions.kt
@@ -2,5 +2,14 @@ package com.example.bitsocurrency.utils.extensions
 
 import androidx.appcompat.widget.AppCompatImageView
 import com.bumptech.glide.Glide
+import java.text.NumberFormat
 
 fun AppCompatImageView.loadUrl(url: String) = Glide.with(context).load(url).into(this)
+
+fun Double.formatAsCurrency(): String {
+    return NumberFormat.getCurrencyInstance().format(this)
+}
+
+fun Double.formatAsPercent(): String {
+    return NumberFormat.getPercentInstance().format(this)
+}

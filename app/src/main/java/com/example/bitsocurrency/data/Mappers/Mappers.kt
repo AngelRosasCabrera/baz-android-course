a3523ceb7fdbd0deabe6e5ff54c509fb8db3516f
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-26 11:48:47 -0500

    Agregando modelos y mappers de Ticker y detalles.

--- app/src/main/java/com/example/bitsocurrency/data/Mappers/Mappers.kt
+++ app/src/main/java/com/example/bitsocurrency/data/Mappers/Mappers.kt
@@ -2,7 +2,11 @@ package com.example.bitsocurrency.data.mappers
 
 import com.example.bitsocurrency.data.database.entities.BitsoEntity
 import com.example.bitsocurrency.data.services.models.bitso.BitsoModel
+import com.example.bitsocurrency.data.services.models.bitso.RollingAverageChangeModel
+import com.example.bitsocurrency.data.services.models.bitso.TickerModel
 import com.example.bitsocurrency.data.services.models.icon.IconResponseItem
+import com.example.bitsocurrency.domain.models.RollingAverageChange
+import com.example.bitsocurrency.domain.models.Ticker
 
 fun BitsoModel.toDatabase(icon: IconResponseItem) = BitsoEntity(
     book = book,
@@ -18,3 +22,19 @@ fun BitsoModel.toDatabase(icon: IconResponseItem) = BitsoEntity(
     name = icon.name,
     symbol = icon.symbol,
 )
+
+fun TickerModel.toDomain() = Ticker(
+    ask = ask,
+    bid = bid,
+    book = book,
+    change24 = change24,
+    createdAt = createdAt,
+    high = high,
+    last = last,
+    low = low,
+    rollingAverageChange = rollingAverageChange.toDomain(),
+    volume = volume,
+    vwap = vwap,
+)
+
+fun RollingAverageChangeModel.toDomain() = RollingAverageChange(x6 = x6)
---
+++ app/src/main/java/com/example/bitsocurrency/domain/models/Details.kt
@@ -0,0 +1,6 @@
+package com.example.bitsocurrency.domain.models
+
+data class Details(
+    val tickers: Ticker,
+    val book: Book
+)

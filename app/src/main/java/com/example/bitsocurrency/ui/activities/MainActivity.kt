c15b7c238db14acad0910d75665b3caac4a2cd3e
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-27 09:30:55 -0500

    Configurando la vista detalles.

--- app/src/main/java/com/example/bitsocurrency/ui/activities/MainActivity.kt
+++ app/src/main/java/com/example/bitsocurrency/ui/activities/MainActivity.kt
@@ -2,20 +2,16 @@ package com.example.bitsocurrency.ui.activities
 
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import com.example.bitsocurrency.R
 import com.example.bitsocurrency.databinding.ActivityMainBinding
-import com.example.bitsocurrency.utils.constants.Constants
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
-    private var backPressedTime: Long = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,22 +21,8 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
     }
 
-    fun showLoading() {
-        binding.lottieLoading.visibility = View.VISIBLE
-    }
-
     fun hideLoading() {
         binding.lottieLoading.visibility = View.GONE
     }
 
-    override fun onBackPressed() {
-        if (backPressedTime + Constants.TIME_TO_EXIT > System.currentTimeMillis()) {
-            super.onBackPressed()
-            finish()
-            return
-        } else {
-            Toast.makeText(this, getString(R.string.press_back_again_to_exit), Toast.LENGTH_SHORT).show()
-        }
-        backPressedTime = System.currentTimeMillis()
-    }
 }
--- app/src/main/java/com/example/bitsocurrency/ui/fragments/DetailsBitsoFragment.kt
+++ app/src/main/java/com/example/bitsocurrency/ui/fragments/DetailsBitsoFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.example.bitsocurrency.R
 import com.example.bitsocurrency.databinding.FragmentDetailsBitsoBinding
-import com.example.bitsocurrency.ui.activities.MainActivity
+import com.example.bitsocurrency.domain.models.Bitso
 import com.example.bitsocurrency.ui.adapters.BitsoTabAdapter
 import com.example.bitsocurrency.ui.viewmodel.BitsoViewModel
 import com.example.bitsocurrency.utils.extensions.formatAsCurrency
@@ -28,35 +31,41 @@ class DetailsBitsoFragment: Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        cardViewDetails(args.bitso)
+        setTabLayout()
+        onBackPressed()
+    }
 
-        val data = args.bitso
-
-        with(binding.clDetailsBitso) {
-            tvDetailsCurrencyName.text = data.name
-            tvDetailsCurrencySymbol.text = data.symbol
-        }
-
+    private fun cardViewDetails(data: Bitso) {
         viewModel.getDetails(data.book)
-
         viewModel.tickers.observe(viewLifecycleOwner) {
             with(binding.clDetailsBitso) {
+                tvDetailsCurrencyName.text = data.name
+                tvDetailsCurrencySymbol.text = data.symbol
                 tvDetailsMaximumPrice.text = it.high.toDouble().formatAsCurrency()
                 tvDetailsMinimumPrice.text = it.low.toDouble().formatAsCurrency()
                 tvDetailsLastPrice.text = it.last.toDouble().formatAsCurrency()
             }
         }
+    }
+
+    private fun setTabLayout() {
         bitsoTabAdapter = BitsoTabAdapter(this)
         binding.pager.adapter = bitsoTabAdapter
         TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
-            tab.text = if (position == 0) "Ask" else "Bids"
+            tab.text = if (position == 0) getString(R.string.tab_ask) else getString(R.string.tab_bid)
         }.attach()
-
-//        binding.btnBack.setOnClickListener { findNavController().popBackStack() }
     }
 
-    fun hideLoading() = (requireActivity() as MainActivity).hideLoading()
+    private fun onBackPressed() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
 
-    fun showLoading() = (requireActivity() as MainActivity).showLoading()
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, callback)
+    }
 
     override fun onDestroy() {
         viewModel.onDestroy()

5aee6f5986c0995f5a7eee9a1114c2a83c058dbd
Miguel Angel Rosas Cabrera <miguel.rosasc@elektra.com.mx>
2022-09-28 14:07:06 -0500

    Cambiando de object a clase NetworkUtils e inyectandola al repositorio.

--- app/src/main/java/com/example/bitsocurrency/utils/network/NetworkUtils.kt
+++ app/src/main/java/com/example/bitsocurrency/utils/network/NetworkUtils.kt
@@ -2,15 +2,18 @@ package com.example.bitsocurrency.utils.network
 
 import android.content.Context
 import android.net.ConnectivityManager
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.coroutineScope
 import java.io.IOException
 import java.net.InetSocketAddress
 import java.net.Socket
 import java.net.SocketTimeoutException
 import java.nio.channels.IllegalBlockingModeException
+import javax.inject.Inject
 
-object NetworkUtils {
-    suspend fun isNetworkAvailable():Boolean = coroutineScope {
+class NetworkUtils @Inject constructor(@ApplicationContext val context: Context) {
+
+    suspend fun isNetworkAvailable(): Boolean = coroutineScope {
         return@coroutineScope try {
             val socket = Socket()
             val address = InetSocketAddress("8.8.8.8", 53)
@@ -19,17 +22,23 @@ object NetworkUtils {
                 socket.close()
             }
             result.isSuccess
+        } catch (e: Exception) {
+            false
+        } catch (e: IOException) {
+            false
+        } catch (e: SocketTimeoutException) {
+            false
+        } catch (e: IllegalBlockingModeException) {
+            false
+        } catch (e: IllegalArgumentException) {
+            false
         }
-        catch (e: Exception) { false }
-        catch (e: IOException) { false }
-        catch (e: SocketTimeoutException) { false }
-        catch (e: IllegalBlockingModeException) { false }
-        catch (e: IllegalArgumentException) { false }
     }
 
-    fun isNetworkConnected(context: Context): Boolean {
+    fun isNetworkConnected(): Boolean {
         val cm = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val netInfo = cm.activeNetworkInfo
         return netInfo != null && netInfo.isConnectedOrConnecting
     }
+
 }
